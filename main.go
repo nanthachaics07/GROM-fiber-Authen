db027e86ecdbe0e2a28578a4b4c77dc83cdb980a
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-27 11:38:35 +0700

    connect to db used grom

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "myuser"
+	password = "mypassword"
+	dbname   = "mydatabase"
+)
+
+func main() {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to Postgres successfully : ", db)
+}
